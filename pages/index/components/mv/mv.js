c9e31d8201bb55d4813ba414c2090a16d52d8269
wulianarashi <44726339+wulianarashi@users.noreply.github.com>
2020-05-21 17:09:42 +0800

    Update mv.js

--- pages/index/components/mv/mv.js
+++ pages/index/components/mv/mv.js
@@ -12,10 +12,7 @@ Component({
   observers: {
     mv_list(item) {
       this.setData({
-        mv_copy_list: item.map(item => {
-          item.playTime = this.tranNumber(item.playTime, 2)
-          return item
-        })
+        mv_copy_list: item.map(item => item)
       })
     }
   },
@@ -32,19 +29,6 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    //对播放量数字的格式化
-    tranNumber(num, point) {
-      let numStr = num.toString().split('.')[0]
-      if (numStr.length < 6) {
-        return numStr
-      } else if (numStr.length >= 6 && numStr.length <= 8) {
-        let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + point)
-        return parseFloat(parseInt(num / 10000) + '.' + decimal) +
-          '万'
-      } else if (numStr.length > 8) {
-        let decimal = numStr.substring(numStr.length - 8, numStr.length - 8 + point)
-        return parseFloat(parseInt(num / 100000000) + '.' + decimal) + '亿'
-      }
-    }
+  
   }
 })
